7820223604fbdad78f543121b52ca1cf2783bd9b
singularityDev <abhinavreddy9681@gmail.com>
2020-07-18 01:03:38 +0530

    case hack

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+// import PropTypes from “prop-types”
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+// import Img from “gatsby-image”
+
+import {
+  NavBar,
+  NavContent,
+  NavLinks,
+  NavLink,
+  NavLinkJoin,
+  NavLogo,
+} from "../design/Styles"
+
+export const Header = () => {
+  const pinkLogo = useStaticQuery(graphql`
+    query {
+      pinkLogo: file(relativePath: { eq: “pinkLogo.png” }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <NavBar>
+      <NavContent>
+        <NavLogo to="/">
+          <img src={pinkLogo.pinkLogo.publicURL} alt="pinkClubLogo" />
+        </NavLogo>
+        <NavLinks>
+          <NavLink className="about" to="/#about" activeClassName="active">
+            about
+          </NavLink>
+          <NavLink className="ourgoal" to="/#ourgoal" activeClassName="active">
+            our goal
+          </NavLink>
+          <NavLink className="ourteam" to="/#ourteam" activeClassName="active">
+            our team
+          </NavLink>
+          <NavLink className="ourwork" to="/#ourwork" activeClassName="active">
+            our work
+          </NavLink>
+          <NavLink className="contact" to="/#contact" activeClassName="active">
+            contact
+          </NavLink>
+        </NavLinks>
+        <NavLinkJoin to="/JoinUs/">join us!</NavLinkJoin>
+      </NavContent>
+    </NavBar>
+  )
+}
+
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
+
+// export default Header
