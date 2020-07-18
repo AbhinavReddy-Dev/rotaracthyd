b32328aeadea1a74d101ce48af7a1a96d647fb4d
singularityDev <abhinavreddy9681@gmail.com>
2020-07-18 12:50:49 +0530

    netlify form hack

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,34 @@ import {
   ContactContent,
   ContactUsForm,
 } from "../design/Styles"
+import { navigate } from "gatsby-link"
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 export const Contact = () => {
+  const [state, setState] = React.useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
   return (
     <SectionWrapper
       bgcolor={props => props.theme.colors.secondaryColor}
@@ -34,43 +61,46 @@ export const Contact = () => {
         </ContactContent>
         <ContactUsForm
           name="contact"
-          method="POST"
+          method="post"
           data-netlify="true"
           netlify-honeypot="bot-field"
-          action="/"
+          action="/thank-you"
+          onSubmit={handleSubmit}
         >
           <h3>Write to us</h3>
-          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" onChange={handleChange} />
           <label>
             Name
             <input
-              // id="name"
+              id="name"
               type="text"
               name="name"
               placeholder="enter your name"
+              onChange={handleChange}
             />
           </label>
           <label>
             Email
             <input
-              // id="email"
+              id="email"
               type="email"
               name="email"
               placeholder="your@email.com"
+              onChange={handleChange}
             />
           </label>
           <label>
             Message
             <textarea
-              // id="message"
+              id="message"
               name="message"
               placeholder="type in your message"
+              onChange={handleChange}
             />
           </label>
           <div data-netlify-captcha></div>
-          <button type="submit" value="SEND MAIL">
-            SEND MAIL
-          </button>
+          <button type="submit">SEND MAIL</button>
         </ContactUsForm>
       </ContactSection>
     </SectionWrapper>
